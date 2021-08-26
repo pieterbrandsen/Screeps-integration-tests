cd72bedc3b0622f52fc54108c00eb17c78b3fa6c
Pieter <pieterbrandsen2@gmail.com>
2021-08-26 11:14:03 +0200

    Commented 2 rooms so server runs faster

--- server/testConfig.js
+++ server/testConfig.js
@@ -8,12 +8,12 @@ module.exports.playerRoom = 'W6N1';
 players = {
   'W6N1': {x: 43, y: 35},
   'W8N3': {x: 21, y: 28},
-  'W7N4': {x: 33, y: 13},
-  'W1N8': {x: 10, y: 9}
+  // 'W7N4': {x: 33, y: 13},
+  // 'W1N8': {x: 10, y: 9}
 };
 module.exports.players = players;
 module.exports.rooms = Object.keys(players);
 
 module.exports.milestones = [
-  {tick: 30, check: {structures: 1}},
+  {tick: 30, check: {creeps: 1}},
 ];
